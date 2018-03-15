26bf38afb8389f80d37e8cbb804f3e124017a86e
Jestin Joshi <jestinjoshi@gmail.com>
2018-03-15 21:29:49 +0530

    15/3/2018

---
+++ app/src/main/java/com/aldrinj/myapplication/AddMedicinesActivity.java
@@ -0,0 +1,9 @@
+package com.aldrinj.myapplication;
+
+/**
+ * Created by SONY_VAIO on 14-03-2018.
+ */
+
+public class AddMedicinesActivity {
+
+}
--- app/src/main/java/com/aldrinj/myapplication/MedicinesActivity.java
+++ app/src/main/java/com/aldrinj/myapplication/MedicinesActivity.java
@@ -1,8 +1,45 @@
 package com.aldrinj.myapplication;
 
-/**
- * Created by vivek on 11-03-2018.
- */
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
-public class MedicinesActivity {
+import static android.view.View.*;
+
+public class MedicinesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstances) {
+        super.onCreate(savedInstances);
+        setContentView(R.layout.activity_medicines);
+
+        final EditText timeInput = findViewById(R.id.time);
+        timeInput.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mCurrentTime = Calendar.getInstance();
+                int hour = mCurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mCurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(MedicinesActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        timeInput.setText(selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, false);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
+    }
+
+    public void addMedicine(View view) {
+        Intent intent = new Intent(this, AddMedicinesActivity.class);
+        startActivity(intent);
+    }
 }
