69dc602a2ebe87b684c225127f8dc9258ecb8830
aldrinjosey <aldrinjosey@gmail.com>
2018-03-19 23:50:27 +0530

    19/03/2018

--- app/src/main/java/com/aldrinj/myapplication/MedicinesActivity.java
+++ app/src/main/java/com/aldrinj/myapplication/MedicinesActivity.java
@@ -6,10 +6,12 @@ import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
 import static android.view.View.*;
+import static com.aldrinj.myapplication.RegisterActivity.myDataBase;
 
 public class MedicinesActivity extends AppCompatActivity {
 
@@ -18,12 +20,14 @@ public class MedicinesActivity extends AppCompatActivity {
         super.onCreate(savedInstances);
         setContentView(R.layout.activity_medicines);
 
+        myDataBase.execSQL("CREATE TABLE IF NOT EXISTS MedicineTable(Name VARCHAR, Doasge INTEGER, Time1 TIMESTAMP, Time2 TIMESTAMP, Time3 TIMESTAMP, Time4 TIMESTAMP);");
+
         final EditText timeInput = findViewById(R.id.time);
         timeInput.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Calendar mCurrentTime = Calendar.getInstance();
-                int hour = mCurrentTime.get(Calendar.HOUR_OF_DAY);
+                int hour = mCurrentTime.get(Calendar.HOUR);
                 int minute = mCurrentTime.get(Calendar.MINUTE);
                 TimePickerDialog mTimePicker;
                 mTimePicker = new TimePickerDialog(MedicinesActivity.this, new TimePickerDialog.OnTimeSetListener() {
@@ -41,5 +45,16 @@ public class MedicinesActivity extends AppCompatActivity {
     public void addMedicine(View view) {
         Intent intent = new Intent(this, AddMedicinesActivity.class);
         startActivity(intent);
+    }
+
+    public void onSelectEveryday(View view) {
+        CheckBox everyday=findViewById(R.id.everydayCheckbox);
+        View everydaySelect = findViewById(R.id.weekdayCheckbox);
+        if(everyday.isChecked()){
+            everydaySelect.setVisibility(View.GONE);
+        }
+        else {
+            everydaySelect.setVisibility(View.VISIBLE);
+        }
     }
 }
