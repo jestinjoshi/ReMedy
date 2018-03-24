b7e3a0dd18016fbd853faf08a0255fb39ae72211
Jestin Joshi <34907800+jestinjoshi@users.noreply.github.com>
2018-03-24 22:55:54 +0530

    A lot of changes

--- app/src/main/java/com/aldrinj/myapplication/MainActivity.java
+++ app/src/main/java/com/aldrinj/myapplication/MainActivity.java
@@ -1,17 +1,23 @@
 package com.aldrinj.myapplication;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static SQLiteDatabase myDataBase;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDataBase = openOrCreateDatabase("MyDataBase", MODE_PRIVATE, null);
+        myDataBase.execSQL("CREATE TABLE IF NOT EXISTS Medicine(Name VARCHAR,Username VARCHAR,Password VARCHAR,Age INTEGER);");
+        myDataBase.execSQL("INSERT INTO Medicine VALUES('Dummy','Dummy','Dummy',0);");
     }
 
     public void toRegister(View view) {
--- app/src/main/java/com/aldrinj/myapplication/MedicinesActivity.java
+++ app/src/main/java/com/aldrinj/myapplication/MedicinesActivity.java
@@ -7,22 +7,24 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-import static android.view.View.*;
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-import static com.aldrinj.myapplication.RegisterActivity.myDataBase;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.aldrinj.myapplication.MainActivity.myDataBase;
 
 public class MedicinesActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstances) {
@@ -30,6 +32,22 @@ public class MedicinesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_medicines);
 
         myDataBase.execSQL("CREATE TABLE IF NOT EXISTS MedicineTable(Name VARCHAR, Doasge INTEGER, Time1 TIMESTAMP, Time2 TIMESTAMP, Time3 TIMESTAMP, Time4 TIMESTAMP);");
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_save,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.save){
+
+        }
+            return super.onOptionsItemSelected(item);
     }
 
     public void addMedicine(View view) {
@@ -38,19 +56,17 @@ public class MedicinesActivity extends AppCompatActivity {
     }
 
     public void onSelectEveryday(View view) {
-        CheckBox everyday=findViewById(R.id.everydayCheckbox);
+        CheckBox everyday = findViewById(R.id.everydayCheckbox);
         View everydaySelect = findViewById(R.id.weekdayCheckbox);
-        if(everyday.isChecked()){
+        if (everyday.isChecked()) {
             everydaySelect.setVisibility(View.GONE);
-        }
-        else {
+        } else {
             everydaySelect.setVisibility(View.VISIBLE);
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void timeInput(final View view) {
-
         Calendar mCurrentTime = Calendar.getInstance();
         int hour = mCurrentTime.get(Calendar.HOUR);
         int minute = mCurrentTime.get(Calendar.MINUTE);
@@ -58,10 +74,26 @@ public class MedicinesActivity extends AppCompatActivity {
         mTimePicker = new TimePickerDialog(MedicinesActivity.this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                ((EditText)view).setText(selectedHour + ":" + selectedMinute);
+                ((Button) view).setText(selectedHour % 12 + ":" + selectedMinute + " " + ((selectedHour >= 12) ? "PM" : "AM"));
+                switch (view.getTag().toString()) {
+                    case "time1":
+                        Button editText = findViewById(R.id.time2);
+                        editText.setVisibility(View.VISIBLE);
+                        break;
+                    case "time2":
+                        Button editText1 = findViewById(R.id.time3);
+                        editText1.setVisibility(View.VISIBLE);
+                        break;
+                    case "time3":
+                        Button editText2 = findViewById(R.id.time4);
+                        editText2.setVisibility(View.VISIBLE);
+                        break;
+                    default:
+                        break;
+                }
             }
-        }, hour, minute, false);//Yes 24 hour time
+        }, hour, minute, false);
         mTimePicker.setTitle("Select Time");
         mTimePicker.show();
     }
-    }
+}
